b527bc6c8a7aff6e27877aed2ece85833de6f7af
calmdowngirl <116081897+calmdowngirl@users.noreply.github.com>
2023-04-09 14:52:32 +0930

    wrapped script in a iffe

--- fun-background/animate-bg.js
+++ fun-background/animate-bg.js
@@ -1,8 +1,11 @@
-const el = document.querySelector('fun-background')
-const arr = '0123456789abcdefghijklmnopqrstuvwxyz'.split('')
-setInterval(() => {
-  let i = parseInt(el.getAttribute('num-prints'))
-  if (i === 9)
-    el.setAttribute('chars', arr[Math.floor(Math.random() * 35)])
-  el.setAttribute('num-prints', i > 5 ? --i : 9)
-}, 450)
+(function () {
+  const el = document.querySelector('fun-background')
+  const arr = '0123456789abcdefghijklmnopqrstuvwxyz'.split('')
+  setInterval(() => {
+    let i = parseInt(el.getAttribute('num-prints'))
+    if (i === 9)
+      el.setAttribute('chars', arr[Math.floor(Math.random() * 35)])
+    el.setAttribute('num-prints', i > 5 ? --i : 9)
+  }, 450)
+})()
+
